c4e871869c3fe28c8b633218ebaca8905ebae9f4
Marilia Faria <mariliafs_@hotmail.com>
2024-08-23 19:48:18 -0300

    aula 8

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+
+
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+
   integrations: [react(), icon()]
 });
