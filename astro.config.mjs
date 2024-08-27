2677c4a9c8ef768371c8effcf4cde4ba6ee9b72e
Marilia Faria <mariliafs_@hotmail.com>
2024-08-27 16:24:54 -0300

    aula 12

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
-
-
 import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-
-  integrations: [react(), icon()]
+  site: 'https://astro-blog-cip.netlify.app',
+  integrations: [react(), icon(), sitemap()]
 });
